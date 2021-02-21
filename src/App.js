5501cacb545907a6a10ae678236ad55f39ea63e2
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2021-02-21 18:45:36 +0500

    created Trello board example

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Board from "@lourenci/react-kanban";
+import "@lourenci/react-kanban/dist/styles.css";
+import { data } from "./data.json";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Trello Board Example</h1>
+        <Board initialBoard={data} />
       </header>
     </div>
   );
